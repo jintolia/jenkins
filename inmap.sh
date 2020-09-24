70f15cda75bb6c450f09209e66682de0345c8f7e
jintolia <68077135+jintolia@users.noreply.github.com>
2020-09-24 21:46:03 +0300

    Update inmap.sh

--- inmap.sh
+++ inmap.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-ssh -i mykey -o StrictHostKeyChecking=no root@192.168.0.20 'yum install nmap'
+chmod 600 mykey
+ ssh -i mykey -o StrictHostKeyChecking=no root@192.168.0.20 'yum install nmap'
