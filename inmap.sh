eb8c3bea9b6cefd2977801f4da3ea18c6b895f58
jintolia <68077135+jintolia@users.noreply.github.com>
2020-09-24 21:58:40 +0300

    Update inmap.sh

--- inmap.sh
+++ inmap.sh
@@ -2,4 +2,4 @@
 ping -c2 tut.by
 ping -c2 192.168.0.20
 chmod 600 mykey
- ssh -i mykey -o StrictHostKeyChecking=no root@192.168.0.20 'yum install nmap'
+ ssh -i mykey -o StrictHostKeyChecking=no root@192.168.0.20 'yum install nmap''nmap -sn mail.ru'
