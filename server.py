8d525bec7f36307a74f383e7b089dfc7f85f9d51
jintolia <68077135+jintolia@users.noreply.github.com>
2020-10-01 20:15:34 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python
 
 import socket
- 
- 
-TCP_IP = '0.0.0.0'
-TCP_PORT = 7000
-BUFFER_SIZE = 300  # Normally 1024, but we want fast response
- 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
 
-conn, addr = s.accept()
-print ('Connection address:', addr)
-while 1:
-     data = conn.recv(BUFFER_SIZE)
-     if not data: break
-     print ("You receved:", data)
-     conn.send(data)  # echo
-conn.close()
+PORT = 7000
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(("", PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
