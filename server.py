affe01c682cd15192df9c90d19fd29066bbdc1da
jintolia <68077135+jintolia@users.noreply.github.com>
2020-10-01 17:38:10 +0300

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the port
-server_address = ('localhost', 8080)
+server_address = ('localhost', 9090)
 print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 
