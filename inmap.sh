604e9c174ad2769938f0299122ab6c40519ba62c
jintolia <68077135+jintolia@users.noreply.github.com>
2020-10-15 07:11:14 +0300

    Delete inmap.sh

--- inmap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ping -c2 tut.by
-ping -c2 192.168.0.20
-chmod 600 mykey
- ssh -i mykey -o StrictHostKeyChecking=no root@192.168.0.20 'yum -y install nmap'
- ssh -i mykey -o StrictHostKeyChecking=no root@192.168.0.20 'nmap -sn mail.ru'
- ssh -i mykey -o StrictHostKeyChecking=no root@192.168.0.20 'yum -y remove nmap'
